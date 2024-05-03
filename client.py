9a937b01a7ea44a7ec403c735338592bcf4cb49c
kento-ix <kentoix1105@gmail.com>
2024-05-02 22:30:08 -0700

    update client created UDP newtwork

---
+++ client.py
@@ -0,0 +1,16 @@
+import socket
+import sys
+
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM) #using UDP network socket
+server_address = '/tmp/socket_file'
+print('connecting to {}' .format(server_address))
+
+try:
+    sock.connect(server_address)
+    print('Success connecting to server.')
+except socket.error as err:
+    print(err)
+    sys.exit(1)
+
+
+        
